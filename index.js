c174fd22983f9005b3be797291e6ec8ab36c39c6
Rakshya Pandey <34873597+rakshyaaa@users.noreply.github.com>
2021-09-22 19:12:19 +0545

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+function removeBtnBg(){
+    document.getElementById("resume-btn").style.backgroundColor = "#eff1f3";
+}
+
+function addBtnBg(){
+    document.getElementById("resume-btn").style.backgroundColor = "#d77a61";
+}
